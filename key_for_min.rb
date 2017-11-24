b8a2c9e328976044c017009613d9c5608b5ae80d
Jason Vance <jbvance@gmail.com>
2017-11-24 14:09:03 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
   return nil if !name_hash.any?
-  min_val = nil
+  min_val = 10000000
   ret_key = nil
   name_hash.collect do |key, value|
     if value < min_val
